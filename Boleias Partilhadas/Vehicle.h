4b1c6d93bd52a5dc19cce7b25eb5eac03b6a7c15
joaoalmeida505 <varsaljr@gmail.com>
2016-11-12 23:11:00 +0000

    changes

--- Boleias Partilhadas/Vehicle.h
+++ Boleias Partilhadas/Vehicle.h
@@ -7,6 +7,7 @@
 
 class Vehicle {
 	const int numberSeats = 0;
+	int avaiableSeats;
 	string brand; //vehicle type, only for showing purposes 
 	unsigned int year; //user might not choose this vehicle for this
 	char rate;  //optional. grade from A to F based on vehicle condition
